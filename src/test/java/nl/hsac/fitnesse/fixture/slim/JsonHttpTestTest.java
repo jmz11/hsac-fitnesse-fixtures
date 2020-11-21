5d49ceafc93cf4eaac0d4cf686b33213692770e0
Fried Hoeben <github@hsac.nl>
2020-11-21 08:23:07 +0100

    Faster json generation without changing order

--- src/test/java/nl/hsac/fitnesse/fixture/slim/JsonHttpTestTest.java
+++ src/test/java/nl/hsac/fitnesse/fixture/slim/JsonHttpTestTest.java
@@ -100,12 +100,13 @@ public class JsonHttpTestTest {
     @Test
     public void testPostValuesAsJson() {
         JsonHttpTest jsonHttpTestTest = new JsonHttpTest();
+        jsonHttpTestTest.setValueFor("3", "C");
         jsonHttpTestTest.setValueFor("1", "A");
         jsonHttpTestTest.setValueFor("2", "B");
         XmlHttpResponse req1 = checkCall(url -> jsonHttpTestTest.postValuesAsJsonTo(url));
         assertEquals("POST", jsonHttpTestTest.getResponse().getMethod());
         assertEquals("POST", req1.getMethod());
-        assertEquals("{\"A\":\"1\",\"B\":\"2\"}", req1.getRequest());
+        assertEquals("{\"C\":\"3\",\"A\":\"1\",\"B\":\"2\"}", req1.getRequest());
     }
 
     @Test
